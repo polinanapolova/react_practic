1b8d79c118ff91a5429f3720ccf18108c32cbc52
Polina <napolitano.polina@gmail.com>
2022-11-23 21:30:50 +0300

    new

---
+++ src/components/Product.tsx
@@ -0,0 +1,14 @@
+import { IProduct } from "../models"
+
+interface ProductProps{
+    product: IProduct
+}
+
+export function Product({product}: ProductProps){
+    return(
+        <div className="border py-2 px-2 rounded flex flex-col items-center mb-2">
+           <img src={product.image} className="w-1/6" alt={product.title}/>
+           {product.title }
+        </div>
+    )
+}
