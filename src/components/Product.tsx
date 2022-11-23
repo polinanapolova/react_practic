0b2a59d35d1ce85c6654440325da2d9de8099c64
Polina <napolitano.polina@gmail.com>
2022-11-23 22:16:10 +0300

    add button

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { IProduct } from "../models"
 
 interface ProductProps{
@@ -5,10 +6,28 @@ interface ProductProps{
 }
 
 export function Product({product}: ProductProps){
+    const [details, setDetails] = useState(false)
+
+    const btnClassName = details ? 'bg-blue-400' : 'bg-yellow-400'
+
+    const btnClasses = ['py-2 px-4 border', btnClassName]
+
     return(
         <div className="border py-2 px-2 rounded flex flex-col items-center mb-2">
            <img src={product.image} className="w-1/6" alt={product.title}/>
-           {product.title }
+           <p>{product.title }</p>
+           <p className="font-bold">{product.price}</p>
+           <button 
+            className={btnClasses.join(' ')}
+            onClick={() => setDetails(prev => !prev)}
+           >
+            { details ? 'Hide details' : 'Show details' }
+            </button>
+            
+            {details && <div>
+                <p>{product.description}</p>
+                <p>Rate : <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+            </div>}
         </div>
     )
 }
