f9e9321b1a280e4e75a450f8634b6dd0b8cd6850
Polina <napolitano.polina@gmail.com>
2022-11-24 22:08:31 +0300

    server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
 import { products } from './data/products';
+import { IProduct } from './models';
 
 function App() {
+  
+  //загрузка с сервера
+
+  const [products, setProducts] = useState<IProduct[]>([])
+
+async function fetchProducts(){
+  const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+  setProducts(response.data)
+}
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+  
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product product={ products[0]}/>
-      <Product product={ products[1]}/>
+      {products.map(product => <Product product={product} key={product.id}/>)}
       
     </div>
   )
